b33e1760f85ed8e62d335b5729772a9e6eb7ed7b
olatzelejalde <145786991+olatzelejalde@users.noreply.github.com>
2026-01-10 10:02:51 +0100

    intsignia kopurua kenduta

--- app/domain/intsignia.py
+++ app/domain/intsignia.py
@@ -5,11 +5,9 @@ class Intsignia:
     izena: str
     deskipzioa: str
     helburua: str
-    kopurua: int
 
     def __init__(self, izena: str, deskipzioa: str,
                  helburua: str, kopurua: int):
         self.izena = izena
         self.deskipzioa = deskipzioa
         self.helburua = helburua
-        self.kopurua = kopurua
