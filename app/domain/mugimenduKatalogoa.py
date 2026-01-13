b4d307d72e418f8a28c8acca055a6f7edb8db78d
olatzelejalde <145786991+olatzelejalde@users.noreply.github.com>
2026-01-13 20:45:02 +0100

    aldaketak

--- app/domain/mugimenduKatalogoa.py
+++ app/domain/mugimenduKatalogoa.py
@@ -5,9 +5,13 @@ from app.domain.mugimendua import Mugimendua
 
 @dataclass
 class MugimenduKatalogoa:
+    # Mugimendu guztien zerrenda
     mugimenduak: List[Mugimendua]
+    # Bere burua katalogo nagusi gisa
     nireMotal: "MugimenduKatalogoa"
 
     def __init__(self):
+        # Mugimendu zerrenda hutsa hasieratzen du
         self.mugimenduak = []
+        # Bere burua erreferentzia nagusi gisa ezartzen du
         self.nireMotal = self
